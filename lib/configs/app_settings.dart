7920c53487dbc111f24e65cc5a0baf8eb29fcd63
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2024-06-19 00:36:24 +0100

    preparação para salvar dados localmente

---
+++ lib/configs/app_settings.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppSettings extends ChangeNotifier {
+  late SharedPreferences prefs;
+}
