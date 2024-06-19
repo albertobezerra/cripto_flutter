4568bf0a9f426df605153e9c7d66e0a76bb9d526
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2024-06-19 01:51:29 +0100

    Atualizações para preparar para salvar dados localmente no telefone

--- lib/configs/app_settings.dart
+++ lib/configs/app_settings.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 class AppSettings extends ChangeNotifier {
+   SharedPreferences prefs;
+    Map<String, String> locale = {
+      'locale': 'pt_BR',
+      'name': 'R\$'
+    };
   
 }
