8432b600459c142d43e03199fefebb6097d6cd2e
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2024-06-19 02:18:06 +0100

    Parte das confs para salvar localmente

--- lib/configs/app_settings.dart
+++ lib/configs/app_settings.dart
@@ -3,13 +3,39 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 
 class AppSettings extends ChangeNotifier {
-   SharedPreferences prefs;
+   late SharedPreferences _prefs;
     Map<String, String> locale = {
       'locale': 'pt_BR',
       'name': 'R\$'
     };
 
-    //oremos
-    //oremos 2
-  
+    AppSettings(){
+      _startSettings();
+    }
+
+    _startSettings()async{
+      await _startPreferences();
+      await _readLocale();
+    }
+
+    Future <void> _startPreferences() async{
+      _prefs = await SharedPreferences.getInstance();
+    }
+
+    _readLocale(){
+      final local = _prefs.getString('local') ?? 'pt_BR';
+      final name = _prefs.getString('name') ?? 'R\$';
+      locale = {
+        'locale' : local,
+        'name': name,
+      };
+      notifyListeners();
+    }
+
+    setLocale(String local, String name) async{
+      await _prefs.setString('local', local);
+      await _prefs.setString('name', name);
+      await _readLocale();
+    }
+
 }
